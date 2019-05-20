31a3e5932c9b4e486370a64f700b622459346072
川崎暑士 <kawasaki8910@gmail.com>
2019-05-20 13:03:40 +0900

    add

---
+++ pages/city.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li v-for="city in cities" :key="city.id">{{ city.name }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  async asyncData({ app }) {
+    const res = await app.$axios(
+      "http://www.land.mlit.go.jp/webland/api/CitySearch?area=13"
+    );
+    console.log(res.data.data);
+    return {
+      cities: res.data.data
+    };
+  }
+};
+</script>
